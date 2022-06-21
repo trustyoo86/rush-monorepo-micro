2db0558fb8d4ce0f91407222b116084dbd6e5c78
HanseungYoo <trustyoo86@gmail.com>
2022-06-22 08:39:07 +0900

    :sparkle: next config update

--- apps/root-next-app/next.config.js
+++ apps/root-next-app/next.config.js
@@ -3,27 +3,9 @@ const path = require('path');
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  productionBrowserSourceMaps: true,
   webpack: (config, options) => {
-    config.module.rules = config.module.rules.map(rule => {
-      // `create-react-app` uses `babel-loader` in oneOf
-      if (rule.oneOf) {
-        rule.oneOf.map(oneOfRule => {
-          if (
-            oneOfRule.loader &&
-            oneOfRule.loader.indexOf('babel-loader') !== -1
-          ) {
-            if (oneOfRule.hasOwnProperty('options')) {
-              if (oneOfRule.options.hasOwnProperty('sourceMaps')) {
-                // eslint-disable-next-line no-param-reassign
-                oneOfRule.options.sourceMaps = true;
-              }
-            }
-          }
-        });
-      }
-      return rule;
-    });
-
+    config.devtool = 'source-map';
     return config;
   },
 };
